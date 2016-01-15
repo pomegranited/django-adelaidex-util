8b07008a960ab044981c5b7f35e2158d674c8c63
Jill Vogel <jill.vogel@adelaide.edu.au>
2016-01-15 12:05:28 +1030

    ADX-206 fixed warning from NullableCharField RemovedInDjango110Warning: SubfieldBase has been deprecated.

--- django_adelaidex/util/fields.py
+++ django_adelaidex/util/fields.py
@@ -5,7 +5,7 @@ from django.db.models import BooleanField
 # http://bitofpixels.com/blog/unique-on-charfield-when-blanktrue/
 class NullableCharField(models.CharField):
     description = "CharField that stores NULL but returns ''"
-    __metaclass__ = models.SubfieldBase
+
     def to_python(self, value):
         return value or ''
     def get_prep_value(self, value):
